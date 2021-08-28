f698b849d11db878722343c2d4009a863290e673
User <ilqarazizoff@gmail.com>
2021-08-28 11:49:54 +0400

    Ilk yukleme

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/welcome', function(){
+    return "Welcome";
+});
+
+Route::get('/api/v1/welcome', function(){
+    return ['mesaj'=>'welcome'];
+});
+
+Route::get('/mehsul/{mehsuladi}/{id?}', function($mehsuladi, $id=0){
+    return "Mehsul adi: $id $mehsuladi";
+})->name('mehsul_etrafli');
+
+Route::get('/kampanya', function(){
+    return redirect()->route('mehsul_etrafli', ['ad'=>'alma', 'id'=>'5']);
+});
